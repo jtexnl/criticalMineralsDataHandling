e2cb1ad85e380d4b2d51745bf3f21bf480768381
jtexnl <jtexnl@gmail.com>
2016-06-04 13:40:15 -0400

    completing data pull script

--- rawDataFiles/pullRawData.py
+++ rawDataFiles/pullRawData.py
@@ -1,30 +1,53 @@
 import requests as rq 
 from bs4 import BeautifulSoup
-import textract
+import os
+import wget
 
 res = rq.get('http://minerals.usgs.gov/minerals/pubs/historical-statistics/')
 d = res.text 
 soup = BeautifulSoup(d, 'lxml')
 
-criticalMinerals = ['quartz', 'cassiterite', 'bauxite', 'bastnaesite', 'copper', 'arsenopyrite', 'silver', 'tantalite', 'wolframite', 'silicon', 'spodumene', 'tungsten', 'tantalum', 'lithium', 'rare earths', 'arsenic', 'aluminum', 'aluminium', 'gallium', 'tin', 'indium']
+criticalMinerals = ['quartz', 'quartz crystal (industrial)', 'cassiterite', 'bauxite', 'bastnaesite', 'copper', 'arsenopyrite', 'silver', 'tantalite', 'wolframite', 'silicon', 'spodumene', 'tungsten', 'tantalum', 'lithium', 'rare earths', 'arsenic', 'aluminum', 'aluminium', 'gallium', 'tin', 'indium']
 
 table = soup.findAll('tbody')
-tableSoup = BeautifulSoup(table)
+tableSoup = BeautifulSoup(str(table))
 rows = tableSoup.findAll('tr')
+
 linksToExtract = {}
+
 for row in rows:
     temp = BeautifulSoup(str(row))
     try:
         subject = temp.find('a').text
-        print ('subject: ' + subject)
         if subject.lower() in criticalMinerals:
-            print('Match!!!')
             linkList = []
             links = temp.findAll('a', href = True)  
             for i in links:
                 linkList.append(i['href'])
-            linksToExtract[subject] = linkList
+            subjectNew = subject.replace('(', '').replace(')', '')
+            linksToExtract[subjectNew] = linkList
         else:
             continue
     except AttributeError:
         continue
+
+def pullFile (link):
+    baseURL = 'http://minerals.usgs.gov/minerals/pubs/historical-statistics/'
+    wget.download(baseURL + link)
+
+os.mkdir('Tin')
+
+for key, value in linksToExtract.items():
+    if not key in os.getcwd():
+        os.mkdir(key.replace(' ', '_'))
+        os.chdir(key.replace(' ', '_'))
+        files = linksToExtract[key]
+        for i in files:
+            pullFile(i)
+        os.chdir('..')
+    else:
+        os.chdir(key.replace(' ', '_'))
+        files = linksToExtract[key]
+        for i in files:
+            pullFile(i)
+        os.chdir('..')
